39dc32834a3b2c2ccf2c48fcedf772694da5016e
Betsegaw Sebsibe <betsesebe@gmail.com>
2022-12-27 16:32:36 +0300

    Add Navigation routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './component/About';
 import Navbar from './component/Navbar';
 import Personaldetail from './component/Personaldetail';
+import Navigation from './component/Navigation';
+import Resume from './component/Resume';
 
 function App() {
   return (
     <>
-      <div className="h-full bg-gradient-to-r from-[#f0cded] via-[#dce5ee] to-[#def5eb] px-[10%] py-[4%]">
-        <Navbar />
-        <div className="grid grid-cols-1  lg:grid-cols-12 gap-10 justify-between mt-[300px]">
-          <div className="lg:col-span-5">
-            <Personaldetail />
+      <Router>
+        <div className="h-full bg-gradient-to-r from-[#f0cded] via-[#dce5ee] to-[#def5eb] px-[5%] py-[4%]">
+          <Navbar />
+          <div className="grid grid-cols-1  lg:grid-cols-12 gap-10 justify-between mt-[300px]">
+            <div className="lg:col-span-5">
+              <Personaldetail />
+            </div>
+            <div className="lg:col-span-7">
+              <div className="flex justify-end">
+                <div className=" w-[30rem]">
+                  <Navigation />
+                </div>
+              </div>
+              <Routes>
+                <Route path="/" element={<About />} />
+                <Route path="/Resume" element={<Resume />} />
+              </Routes>
+            </div>
           </div>
-          <div className="lg:col-span-7">hello</div>
         </div>
-      </div>
+      </Router>
     </>
   );
 }
